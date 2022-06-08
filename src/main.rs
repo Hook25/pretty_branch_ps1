e129084937161a62475c85ee1383b5c28f1a82d4
h25 <>
2022-06-08 14:06:48 +0200

    Removed newlines and fixed else

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,16 @@ Reads branch from stdin
 Useful for PS1 when using gitlab where the default name
 for MR is NNN-issue-name
 */
+
+//function from: https://stackoverflow.com/questions/37888042/remove-single-trailing-newline-from-string-without-cloning
+fn trim_newline(s: &mut String){
+  if s.ends_with('\n') {
+      s.pop();
+      if s.ends_with('\r') {
+          s.pop();
+      }
+  }
+}
 fn main() -> io::Result<()> {
   let mut branch_name = String::new();
   io::stdin().read_line(&mut branch_name)?;
@@ -15,10 +25,15 @@ fn main() -> io::Result<()> {
   if c_iter.clone().count() > 0 {
     let start_c = c_iter.next().unwrap();
     if start_c.is_digit(10){
-      print!("{}-...", branch_name.split('-').collect::<Vec<&str>>()[0]);
+      let mut branch_number = branch_name.split('-')
+        .collect::<Vec<&str>>()[0].to_string();
+      trim_newline(&mut branch_number);
+      print!("{}-...", branch_number);
+    }else{
+      let mut branch_name_stripped = branch_name.to_string();
+      trim_newline(&mut branch_name_stripped);
+      print!("{}", branch_name_stripped);
     }
-  }else{
-    print!("{}", branch_name);
   }
   Ok(())
 }
